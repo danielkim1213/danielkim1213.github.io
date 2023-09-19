259d0c9c6a4911678076783af8989de48d4b4635
Daniel Kim <kim506@mcmaster.ca>
2023-09-19 15:58:56 -0400

    background

---
+++ index.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById('Matrix');
+const context = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const latin = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const number = '1234567890';
+const special = '!@#$%^&*';
+
+const alphabet = latin + number + special;
+
+const fontSize = 16;
+const columns = canvas.width/fontSize;
+
+const raindrop = [];
+
+for(let x = 0; x < columns; x++)
+{
+    raindrop[x] = 1;
+}
+
+const draw = () => {
+    context.fillStyle = 'rgba(0,0,0,0.04)';
+    context.fillRect(0,0,canvas.width, canvas.height);
+    
+    context.fillStyle = '#0F0';
+    context.font = fontSize + 'px monospace';
+
+    for(let i = 0; i< raindrop.length; i++)
+    {
+        const text = alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+        context.fillText(text, i*fontSize, raindrop[i]*fontSize);
+
+        if(raindrop[i]*fontSize > canvas.height && Math.random() > 0.975)
+        {
+            raindrop[i] = 0;
+        }
+        raindrop[i]++;
+    }
+};
+
+setInterval(draw, 30);
