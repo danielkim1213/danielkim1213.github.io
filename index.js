5c874c61af7233f142bde89def165c7c00108386
Daniel Kim <kim506@mcmaster.ca>
2023-09-20 10:58:27 -0400

    msg

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ for(let x = 0; x < columns; x++)
 
 const draw = () => {
 
-    context.fillStyle = 'rgba(0,0,0,0.04)';
+    context.fillStyle = 'rgba(0,0,0,0.05)';
     context.fillRect(0,0,canvas.width, canvas.height);
     
     context.fillStyle = '#0F0';
@@ -41,6 +41,6 @@ const draw = () => {
     }
 };
 
-setInterval(draw, 30);
+setInterval(draw, 35);
 
 window.onresize = function(){ location.reload(); }
