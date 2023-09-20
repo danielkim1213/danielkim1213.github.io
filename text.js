dea422360c2d8d5605b085f86735e853566fbd3a
Daniel Kim <kim506@mcmaster.ca>
2023-09-20 16:58:33 -0400

    text writer

--- text.js
+++ text.js
@@ -1,4 +1,21 @@
-const text = "Hello, this is a typing animation!";
+const text = "Name: Yewan Kim\n"
+            + "Preferred Name: Daniel\n"
+            + "Gender: Male\n"
+            + "Age: 18\n"
+            + "Academic career: Computer Science @ McMaster University\n"
+            + "Year of Study: 2\n\n"
+            + "Skills: \n"
+            + "- Java\n"
+            + "- Python\n"
+            + "- HTML/CSS\n"
+            + "- JavaScript\n"
+            + "- C#\n"
+            + "- C\n"
+            + "- SAP ABAP\n"
+            + "- Video Editing\n"
+
+
+
 const textContainer = document.getElementById("text-container");
 const typeButton = document.getElementById("type-button");
 let currentIndex = 0;
@@ -7,18 +24,27 @@ let currentIndex = 0;
 
 function typeText() {
     if (currentIndex < text.length) {
+        textContainer.innerHTML = textContainer.innerHTML.substring(0,textContainer.innerHTML.length-1);
         textContainer.innerHTML += text.charAt(currentIndex);
         if(text.charAt(currentIndex) == " ")
         {
-            setTimeout(typeText, 150);
+            setTimeout(typeText, 50);
+        }
+        else if(text.charAt(currentIndex) == "\n")
+        {
+            setTimeout(typeText, 300);
+            textContainer.innerHTML += ("<br>");
         }
         else
         {
-            setTimeout(typeText, 30 + Math.random()*60);
+            setTimeout(typeText, 40);
         }
         currentIndex++;
+        textContainer.innerHTML += "|";
     } else {
         typeButton.disabled = false; 
+        textContainer.innerHTML = textContainer.innerHTML.substring(0,textContainer.innerHTML.length-1);
+        textContainer.innerHTML += "<br>";
     }
 }
 
